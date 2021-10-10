6617c77ee6137f7241871f1bfd4c9845f216270b
Michal Bradac <m.bradac.michal@gmail.com>
2021-10-10 22:31:03 +0200

    improve php script

--- montmail.php
+++ montmail.php
@@ -5,16 +5,18 @@ $subject = $_POST["inputsubject"];
 $message = $_POST["inputmessage"];
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
-$email = $_POST["inputemail"]
+$email = $_POST["inputemail"];
+$headers =      'From: montana@alter.cz'           . "\r\n" .
+                'Reply-To: ' + $email              . "\r\n" .
+                'X-Mailer: PHP/'                   . phpversion();
 
-$body = "Od: " + $firstname + " " + $lastname + " <" + $email + ">\n" + $message
 
-if (@mail($to, $subject, $body, $headers)):
+$body = "Od: " + $firstname + " " + $lastname + " <" + $email + ">\n" + $message;
+
+if (mail($to, $subject, $body, $headers)){
         http_response_code(200);
         return;
-else:
-        http_response_code(500);
-        return;
-endif;
+}
 
+http_response_code(500);
 ?>
